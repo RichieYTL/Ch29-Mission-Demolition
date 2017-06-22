93bbb90100cb349e109e8548f52e0142c31e8088
Richie Nguyen <richieytl@gmail.com>
2017-06-22 00:47:06 -0400

    Created a Unity .gitignore file. Added projectile instantiation to slingshot script.

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
 
+    public GameObject prefabProjectile;
 	public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    public Rigidbody rb;
 
 	void Awake(){
 		Transform launchPointTrans = transform.Find ("LaunchPoint");
 		launchPoint = launchPointTrans.gameObject;
 		launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
 	}
 
 	void OnMouseEnter(){
@@ -22,4 +28,12 @@ public class Slingshot : MonoBehaviour {
 		launchPoint.SetActive(false);
 	}
 
+    private void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        rb = projectile.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
 }
